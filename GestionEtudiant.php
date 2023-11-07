74eb4ff1d9b2225da5e75ccfe372a90846e7d171
sancty <sanctifieryawmensah55@gmail.com>
2023-11-08 00:31:52 +0100

    Affiche nom des etudiants

--- GestionEtudiant.php
+++ GestionEtudiant.php
@@ -1,2 +1,24 @@
 <?php
 
+$etudiants =[
+    [
+        'nom'=>     "Doe",
+        'prenom'=>  "john",
+        'notes' => [10,12,10,9]
+    ],
+    [
+        'nom'=>     "Marc",
+        'prenom'=>  "yong",
+        'notes' => [10,12,10,9]
+    ],
+    [
+        'nom'=>     "Pierre",
+        'prenom'=>  "LePain",
+        'notes' =>[10,12,10,9]
+    ]
+];
+
+
+for($i =0 ; $i <count($etudiants); $i++){
+    echo "le nom de l'eleve $i est : ".$etudiants[$i]['nom'] ."\n";
+}
--- demo.php
+++ demo.php
@@ -1,13 +1,13 @@
 <?php
 
-    $prenom = "Marc";
-    $nom    = "Doe";
-    //simplication du code 
-    $notes =[10,20];
-    $somme = 0;
-    for ($i = 0; $i <= count($notes); $i++) {
-       $somme +=$i; 
-    }
-    $moyenne = $somme / count($notes);
-    
-    echo "Bonjour $prenom $nom vous avez eu $moyenne de moyenne";
+        $prenom = "Marc";
+        $nom    = "Doe";
+        //simplication du code 
+        $notes =[10,20];
+        $somme = 0;
+        for ($i = 0; $i <= count($notes); $i++) {
+        $somme +=$i; 
+        }
+        $moyenne = $somme / count($notes);
+        
+        echo "Bonjour $prenom $nom vous avez eu $moyenne de moyenne";
