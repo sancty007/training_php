a1389d1484dea13d29eb5cfbdd477a54dd032050
sancty <sanctifieryawmensah55@gmail.com>
2023-11-16 02:18:19 +0100

    Ajout fichier Exemple.php

---
+++ contact.php
@@ -0,0 +1,5 @@
+
+<?php "header.php"; ?>
+<p>nous contacter</p>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae ut, iure sint, quae cumque molestiae nobis error porro sequi quibusdam odit quod ex adipisci illo consectetur alias pariatur est voluptates.</p>
+<?php "footer.php"; ?>
